eff3a21e797bbf40741dd3b8ecee91ff9753dee7
VANDALLA7 <nk0211nastya@gmail.com>
2026-05-31 18:03:55 +0500

    первая загрузка проекта

---
+++ script/script.js
@@ -0,0 +1,85 @@
+const navLinks = document.querySelectorAll('.header__nav-item a');
+const nav = document.querySelector('.header__nav'); // Это тег <ul>
+
+const line = document.createElement('div');
+line.className = 'nav-line';
+line.style.cssText = `
+    position: absolute;
+    bottom: 0;
+    height: 4px;
+    width: 35px;
+    background-color: #C4C4C4;
+    border-radius: 6px;
+    transition: all 0.4s ease;
+`;
+nav.appendChild(line);
+
+function moveLine(activeLink) {
+    const linkCenter = activeLink.offsetLeft + (activeLink.offsetWidth / 2);
+    const lineLeft = linkCenter - (35 / 2);
+    line.style.left = lineLeft + 'px';
+}
+
+const activeLink = document.querySelector('.header__nav-item a.active');
+if (activeLink) {
+    moveLine(activeLink);
+}
+
+navLinks.forEach(link => {
+    link.addEventListener('mouseenter', () => {
+        moveLine(link);
+    });
+});
+
+nav.addEventListener('mouseleave', () => {
+    if (activeLink) {
+        moveLine(activeLink);
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const track = document.querySelector('.slider__pics');
+  const slides = document.querySelectorAll('.slide');
+  const btn = document.querySelector('.slider__btn');
+
+  if (!track || slides.length === 0 || !btn) return;
+
+  let currentIndex = 1;
+  const slideWidth = 300;
+  const gap = 1;
+
+  function updateCarousel() {
+    const offset = currentIndex * (slideWidth + gap);
+    track.style.transform = `translateX(-${offset}px)`;
+
+    slides.forEach((slide, index) => {
+      slide.classList.toggle('active', index === currentIndex);
+    });
+  }
+
+  btn.addEventListener('click', () => {
+    currentIndex++;
+    if (currentIndex >= slides.length) {
+      currentIndex = 0;
+    }
+    updateCarousel();
+  });
+
+  updateCarousel();
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('subscribeForm');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value.trim();
+    const tel = document.getElementById('tel').value.trim();
+
+    alert('Вы успешно подписались на рассылку!');
+    form.reset();
+  });
+});
